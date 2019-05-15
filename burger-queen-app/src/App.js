86e92ba8a9c02668eb5fceab6dfac0cf68e2a0a7
unknown <anafrs26@gmail.com>
2019-05-15 16:47:42 -0500

    Install framework  materilaze

--- burger-queen-app/src/App.js
+++ burger-queen-app/src/App.js
@@ -1,43 +1,40 @@
-import React from 'react'; //importar componente de react
-import burgerLogo from './burgerLogo.png'; //declarar una variable para importar la imagen
-import './App.css';
-import 'materialize-css/dist/css/materialize.min.css';
+import React from "react"; //importar componente de react
+
+// import { Link } from "react-router-dom"; // router, you need to install
+import burgerLogo from "./burgerLogo.png"; //declarar una variable para importar la imagen
+import "./App.css";
+import "materialize-css/dist/css/materialize.min.css";
 
 function App() {
   return (
     <div className="App">
-      <nav className="orange">
+      <nav className="brown"> 
         <div className="nav-wrapper">
-        
           <img src={burgerLogo} className="brand-logo center" alt="logo" />
           <a href="#" className="brand-logo center">
-            Welcome
+            ¡Welcome!
           </a>
           <ul id="nav-mobile" className="left hide-on-med-and-down" />
         </div>
       </nav>
 
       <header className="App-header">
-        {/* <img src={burgerLogo} className="App-logo" alt="logo" /> */}
-
-        <p />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        />
-
-        <a className="waves-effect waves-light btn-small">
-          <i className="material-icons left">Waiter</i>
-        </a>
-        <br />
-        <a className="waves-effect waves-light btn-small">
-          <i className="material-icons right">Kitchen</i>
-        </a>
+        
       </header>
+      
+      <a className="waves-effect btn  deep-orange  darken-1">
+          <i className="material-icons right">room_service</i>Waiterb
+          {/* <Link to="/waiter">Waiter</Link>  */}
+        </a>
+
+        <a className="waves-effect brown darken-1 btn">
+          <i className="material-icons right">restaurant_menu</i>kitchen
+          {/* <Link to="/kitchen">Kitchen</Link>  */}
+        </a>
     </div>
   );
 }
 
 export default App;
+
+
---
+++ burger-queen-app/src/kitchen.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import App from "./App";
+
+class Kitchen extends Component {
+    render () {
+        return (
+           <div >
+         <h1>hola estas en kitchen</h1>
+          <App />
+          </div>
+        )
+    }
+}
+
+export default Kitchen;
