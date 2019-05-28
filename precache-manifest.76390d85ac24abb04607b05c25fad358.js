31b79a4149b2cdea1b653a9f0a8c809c7b220984
unknown <anafrs26@gmail.com>
2019-05-28 09:16:54 -0500

    Updates

---
+++ precache-manifest.76390d85ac24abb04607b05c25fad358.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eb6486b6cfb50f86370b97fa27c9309",
+    "url": "/GDL002-burger-queen/index.html"
+  },
+  {
+    "revision": "a3a5df4d550b25c1b06f",
+    "url": "/GDL002-burger-queen/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "59cb6c7494ce2914197d",
+    "url": "/GDL002-burger-queen/static/css/main.ff0777fc.chunk.css"
+  },
+  {
+    "revision": "a3a5df4d550b25c1b06f",
+    "url": "/GDL002-burger-queen/static/js/2.32db89ba.chunk.js"
+  },
+  {
+    "revision": "59cb6c7494ce2914197d",
+    "url": "/GDL002-burger-queen/static/js/main.379cf209.chunk.js"
+  },
+  {
+    "revision": "f8c6079179fe30af807c",
+    "url": "/GDL002-burger-queen/static/js/runtime~main.2601196f.js"
+  },
+  {
+    "revision": "0354cb1c8bbefa6d41096bf23712ec5a",
+    "url": "/GDL002-burger-queen/static/media/logo.0354cb1c.svg"
+  }
+]);
